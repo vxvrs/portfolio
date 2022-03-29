09d99930a382f730ce8028607b044f5c8eef3f99
vxvrs <viggooveres@gmail.com>
2022-03-29 19:35:55 +0200

    Changed email address

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -1,7 +1,7 @@
 <div class="body">
   <p>
     If you have any questions or suggestions, feel free to <a
-      href="mailto:voveres@pm.me">contact me</a
+      href="mailto:mail@viggooveres.xyz">contact me</a
     >.
   </p>
   <!-- _ .__(.)< (MEOW) \___) ~~~~~~~~~duck c/o amazon~~~~~~~~~-->
