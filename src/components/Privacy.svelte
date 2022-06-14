b0083a4246020a880c0e0c9859f332d69f1e3aea
Viggo Overes <58688867+vxvrs@users.noreply.github.com>
2022-06-14 15:59:03 +0200

    Changed app name and contact email

--- src/components/Privacy.svelte
+++ src/components/Privacy.svelte
@@ -1,5 +1,5 @@
 <div class="body">
-  <h1>Privacy Policy Asteroid Gliders</h1>
+  <h1>Privacy Policy Asteroid Glider</h1>
   <p>
     We are not interested in collecting any personal information. We believe
     such information is yours and yours alone. We do not store or transmit your
@@ -9,14 +9,14 @@
 
   <h2>What information Do We Collect?</h2>
   <p>
-    None. We don't collect any data. Asteroid Gliders is a game and your score
+    None. We don't collect any data. Asteroid Glider is a game and your score
     and highscore are stored locally and no where else.
   </p>
 
   <h2>Contact</h2>
   <p>
     If you have any questions or concerns, feel free to <a
-      href="mailto:voveres@pm.me">contact me</a
+      href="mailto:mail@viggooveres.xyz">contact me</a
     >.
   </p>
 </div>
